338cbba36a4b8e1f1e8023cc6e24b4b9785abeef
labasmindaugai <labasmindaugai@gmail.com>
2022-06-08 15:19:30 +0300

    added login form

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LogInPage from './public/log-In-page';
+// import MainPage from './auth/main-page';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<LogInPage/>}></Route>
+      {/* <Route path="/main" element={<MainPage/>}></Route> */}
+    </Routes>
+    </BrowserRouter>
     </div>
   );
 }
